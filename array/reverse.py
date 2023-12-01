b53b904b628e360b38fd25733a26bb5b9c0839ed
Ben Lambert <bennett-spencer.lambert@pierer-innovation.com>
2023-12-01 14:21:39 +0100

    add string reverse

---
+++ array/reverse.py
@@ -0,0 +1,11 @@
+def reverse(input_string: str) -> str:
+    i = len(input_string) - 1
+    result = []
+    while i >= 0:
+        result.append(input_string[i])
+        i -= 1
+    return "".join(result)
+
+
+assert "olleh" == reverse("hello")
+assert "racecar" == reverse("racecar")
