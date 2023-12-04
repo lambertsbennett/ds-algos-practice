866e13be3b63194a4122e74094c04cd91315bc73
Ben Lambert <bennett-spencer.lambert@pierer-innovation.com>
2023-12-04 08:28:56 +0100

    add first_recurring_character.py

---
+++ hashmap/first_recurring_char.py
@@ -0,0 +1,14 @@
+def get_first_recurring_char(input_str: str) -> str:
+    chars = {}
+    # Convert to lower case and remove whitespaces.
+    for c in input_str.lower().replace(" ", ""):
+        if c in chars.keys():
+            return c
+        else:
+            chars[c] = 1
+    return None
+
+
+assert get_first_recurring_char("racecar") == "c"
+assert get_first_recurring_char("The answer") == "e"
+assert get_first_recurring_char("I am happy") == "a"
