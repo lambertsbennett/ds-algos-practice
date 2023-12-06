53a81b9ff97e46a2672790c98ad081a4c6ad6130
Ben Lambert <bennett-spencer.lambert@pierer-innovation.com>
2023-12-06 13:53:07 +0100

    add reversable linked list

--- linked_list/reverse.py
+++ linked_list/reverse.py
@@ -1,1 +1,25 @@
-from ll_implementation import ListNode, LinkedList
+from ll_implementation import LinkedList
+
+
+class ReversibleLinkedList(LinkedList):
+    def reverse(self):
+        if self.length == 1:
+            pass
+        else:
+            first = self.head
+            self.tail = self.head
+            second = first.next()
+            while second:
+                tmp = second.next()
+                second.ref = first
+                first = second
+                second = tmp
+            self.head.ref = None
+            self.head = first
+
+
+rll = ReversibleLinkedList(5)
+rll.append(6)
+rll.append(7)
+rll.reverse()
+rll.traverse(None, v=True)
